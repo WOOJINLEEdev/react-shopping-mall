f125e003034f01b4c3d182a56013390cde0c9157
WOOJINLEEdev <lwj3794@naver.com>
2021-12-04 13:28:05 +0900

    토큰 만료 시 토큰 제거

--- src/utils/http-client.js
+++ src/utils/http-client.js
@@ -15,3 +15,15 @@ instance.interceptors.request.use(
     return Promise.reject(error);
   }
 );
+
+instance.interceptors.response.use(
+  function (response) {
+    if (response.status === 401 || response.status === 403) {
+      localStorage.removeItem("token");
+    }
+    return response;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
+);
