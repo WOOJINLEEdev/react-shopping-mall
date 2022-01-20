9944c7542af7e404a756671bb4d34544904c7a26
WOOJINLEEdev <lwj3794@naver.com>
2022-01-20 14:05:48 +0900

    SelectBoardPage 스타일 개선

--- src/pages/SelectBoardPage.js
+++ src/pages/SelectBoardPage.js
@@ -1,17 +1,31 @@
 import React from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
+import useCurrentBoardPage from "hooks/useCurrentBoardPage";
 
 const SelectBoardPage = () => {
+  const { resetBoardData } = useCurrentBoardPage();
+  const onClickBoardSelect = (boardType) => {
+    resetBoardData(boardType);
+  };
+
   return (
     <SelectWrapper>
       <SelectBoard>
-        <Link to="/selectBoard1" className="select_board_link">
+        <Link
+          to="/selectBoard1"
+          className="select_board_link"
+          onClick={() => onClickBoardSelect("first")}
+        >
           <p className="select_board_text">데이터를 작성하여 만든 게시판</p>
         </Link>
       </SelectBoard>
       <SelectBoard>
-        <Link to="/selectBoard2" className="select_board_link">
+        <Link
+          to="/selectBoard2"
+          className="select_board_link"
+          onClick={() => onClickBoardSelect("second")}
+        >
           <p className="select_board_text">
             JSONPlaceholder의 API를 이용하여 만든 게시판
           </p>
@@ -25,7 +39,7 @@ export default SelectBoardPage;
 
 const SelectWrapper = styled.div`
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
   justify-content: space-between;
   height: 100vh;
   padding: 20px;
@@ -38,19 +52,39 @@ const SelectWrapper = styled.div`
 `;
 
 const SelectBoard = styled.div`
-  width: 100%;
+  position: relative;
+  width: 45%;
+  height: 100%;
   border-radius: 5px;
   line-height: 48vh;
-  background: linear-gradient(to right top, #fff, #d4d4d4);
 
-  @media (hover: hover) {
-    &:hover {
-      border: 1px solid #efefef;
-      box-shadow: 0 0.5rem 1rem rgb(0 0 0 / 20%);
-      background: linear-gradient(to left bottom, #fff, #d4d4d4);
-      transition: all 0.25s;
-      transform: translateY(-2px);
-      font-weight: bold;
+  @media only screen and (min-width: 320px) and (max-width: 767px) {
+    width: 100%;
+    line-height: 45%;
+  }
+
+  & p {
+    position: absolute;
+    width: 100%;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    border: 3px solid green;
+    border-radius: 5px;
+
+    @media only screen and (min-width: 320px) and (max-width: 767px) {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 90%;
+      max-height: 400px;
+    }
+
+    @media (hover: hover) {
+      &:hover {
+        background: rgba(255, 255, 0, 0.1);
+        font-weight: bold;
+      }
     }
   }
 `;
