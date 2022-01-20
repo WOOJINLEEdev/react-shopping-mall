b1784e15b41d8c16e6534d9be3141a8f2510db27
WOOJINLEEdev <lwj3794@naver.com>
2022-01-20 17:53:01 +0900

    SelectBoardPage 컴포넌트 css 수정

--- src/pages/SelectBoardPage.js
+++ src/pages/SelectBoardPage.js
@@ -41,31 +41,40 @@ const SelectWrapper = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: space-between;
-  height: 100vh;
+  min-height: calc(100vh - 211px);
+  height: 100%;
   padding: 20px;
   text-align: center;
   font-size: 20px;
 
   @media only screen and (min-width: 320px) and (max-width: 767px) {
     flex-direction: column;
+    min-height: calc(100vh - 181px);
+    height: 100%;
+  }
+
+  @media only screen and (min-width: 768px) and (max-width: 1023px) {
   }
 `;
 
 const SelectBoard = styled.div`
   position: relative;
-  width: 45%;
+  width: 48%;
+  line-height: 400px;
+  min-height: calc(100vh - 211px);
   height: 100%;
   border-radius: 5px;
-  line-height: 48vh;
 
   @media only screen and (min-width: 320px) and (max-width: 767px) {
     width: 100%;
-    line-height: 45%;
+    min-height: calc(50vh - 105.5px);
+    line-height: 50%;
   }
 
   & p {
     position: absolute;
-    width: 100%;
+    width: 400px;
+    height: 400px;
     top: 50%;
     left: 50%;
     transform: translate(-50%, -50%);
@@ -76,7 +85,16 @@ const SelectBoard = styled.div`
       display: flex;
       justify-content: center;
       align-items: center;
-      height: 90%;
+      width: 100%;
+      height: 100%;
+      max-width: 400px;
+      max-height: 400px;
+    }
+
+    @media only screen and (min-width: 768px) and (max-width: 1023px) {
+      width: 100%;
+      height: 100%;
+      max-width: 400px;
       max-height: 400px;
     }
 
